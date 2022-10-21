ab81244d4cc640c1e298040b1db534fb8834ebe3
Apoquinto <RCLlanesA@gmail.com>
2022-10-20 20:33:33 -0500

    feat(components): generate UserResume
    Generate UserResume component and add a faked async load data.

---
+++ src/components/UserResume.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  const userData = await new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Data loaded");
+      resolve(true);
+    }, 2000);
+  });
+</script>
+
+<template>
+  <main class="grid place-items-center w-screen h-screen">
+    UserHome
+  </main>
+</template>
