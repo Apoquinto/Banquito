a348341ddc08c14a6006cdb1f1996a487911aaa4
Apoquinto <RCLlanesA@gmail.com>
2022-10-20 19:03:20 -0500

    setup project
    Generate project with vite vue-ts template
    Add boxicons as a font
    Add tailwindcss

---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="grid place-items-center w-screen h-screen">
+    <span>Oh no, estoy vivo <i class='bx bx-line-chart text-xl'></i></span>
+  </main>
+</template>
