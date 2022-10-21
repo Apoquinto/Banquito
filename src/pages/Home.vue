1a8536f5f483a879a77effd35c4e3beb7d3781f1
Apoquinto <RCLlanesA@gmail.com>
2022-10-20 20:43:34 -0500

    feat(pages): add splash and resume screen
    Add Async component loading to Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,14 +2,14 @@
 import SplashScreen from '../components/SplashScreen.vue';
 import { defineAsyncComponent } from 'vue'
 
-const UserResume = defineAsyncComponent(() =>
-  import('../components/UserResume.vue')
-)
+const UserResume = defineAsyncComponent(() => import('../components/UserResume.vue'))
 </script>
 
 <template>
-  <SplashScreen/>
-<!--   <main class="grid place-items-center w-screen h-screen">
-    <span>Oh no, estoy vivo <i class='bx bx-line-chart text-xl'></i></span>
-  </main> -->
+  <Suspense>
+    <UserResume/>
+    <template #fallback>
+      <SplashScreen/>
+    </template>
+  </Suspense>
 </template>
