28dac72a5b8389b03a876150862b025c35b23bd8
Apoquinto <RCLlanesA@gmail.com>
2022-10-21 01:50:57 -0500

    feat(components): create Header component
    Generate a simple base header

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  const reload = () => location.reload();
+</script>
+
+<template>
+  <header class="w-full flex justify-between px-5 py-2 border-b">
+     <img src="../assets/icon.svg" alt="logo" @click=reload>
+     <h2 class=" text-indigo-500">Banquito</h2>
+  </header>
+</template>
