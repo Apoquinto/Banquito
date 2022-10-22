954f79984edfcc435bd5e123f1777ac68b1614f6
Apoquinto <RCLlanesA@gmail.com>
2022-10-21 21:29:38 -0500

    feat(components): generate userHistoryAccordeon

---
+++ src/components/UserHistoryAccordion.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="w-full h-fit border-t">
+    <button class="w-full bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-300 border-x rounded-t-xl text-white flex justify-center items-center py-3">
+      <span class="bx bx-math text-2xl pr-2"></span>Show details
+    </button>
+  </section>
+</template>
