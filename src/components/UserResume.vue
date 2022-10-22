67319b0859092ef3e17e90597bc623baca4c86b8
Apoquinto <RCLlanesA@gmail.com>
2022-10-21 21:33:44 -0500

    feat(components): add userHistoryAccordion
    Add UserHistoryAccordion to UserResume

--- src/components/UserResume.vue
+++ src/components/UserResume.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-  const userData = await new Promise((resolve) => {
-    setTimeout(() => {
-      console.log("Data loaded");
-      resolve(true);
-    }, 2000);
-  });
+import UserHistoryAccordion from './UserHistoryAccordion.vue';
+
+const userData = await new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Data loaded");
+    resolve(true);
+  }, 2000);
+});
 </script>
 
 <template>
   <main class="grid place-items-center w-full h-full">
     UserHome
   </main>
+  <UserHistoryAccordion/>
 </template>
