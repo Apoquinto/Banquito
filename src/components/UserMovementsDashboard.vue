11f5a060dba996d2c805ffcc24417bf720f185b8
Apoquinto <RCLlanesA@gmail.com>
2022-10-21 22:57:18 -0500

    feat(Component): generate component layout

---
+++ src/components/UserMovementsDashboard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="layout">
+    <!-- Total, Mountly, Weekly report -->
+    <!-- Data graph -->
+    <!-- Register new movent-->
+  </section>
+</template>
+
+<style scoped>
+  .layout {
+    display: grid;
+    grid-template-rows: min-content auto min-content;
+  }
+</style>
