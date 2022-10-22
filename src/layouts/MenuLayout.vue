c759a51b7c34432f3465172af6b3e47efcd48579
Apoquinto <RCLlanesA@gmail.com>
2022-10-21 21:35:35 -0500

    styles(Layouts): fix MenuLayout
    Implement a custom css style to better responsive behaviour

--- src/layouts/MenuLayout.vue
+++ src/layouts/MenuLayout.vue
@@ -1,6 +1,15 @@
 <template>
-  <main class="w-screen h-screen">
+  <main class="w-screen h-screen layout">
     <slot name="header"></slot>
     <slot></slot>
+    <slot name="history"></slot>
   </main>
 </template>
+
+
+<style scoped>
+  .layout {
+    display: grid;
+    grid-template-rows: min-content auto min-content;
+  }
+</style>
