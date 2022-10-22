06eac0b5c92844acea95507eb20f84c315e426be
Apoquinto <RCLlanesA@gmail.com>
2022-10-21 21:30:55 -0500

    clean(pages): remove useless import
    Remove useless SplashScreen import from pages/Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Header from '../components/Header.vue';
-import SplashScreen from '../components/SplashScreen.vue';
 import { defineAsyncComponent } from 'vue'
 import MenuLayout from "../layouts/MenuLayout.vue"
 
