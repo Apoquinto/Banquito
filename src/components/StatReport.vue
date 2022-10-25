ba140180d78ef8b7eb231bbeb878c391c9ca30e6
Apoquinto <RCLlanesApo@gmail.com>
2022-10-25 09:00:53 -0500

    feat(components): add currency format

--- src/components/StatReport.vue
+++ src/components/StatReport.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { computed, toRefs } from 'vue';
 
 const props = defineProps({
-  amount: Number,
-  label: String
-})
+  amount: {
+    type: Number,
+    default: 0,
+  },
+  label: {
+    type: String,
+    required: true,
+  }
+});
 
 const { amount, label } = toRefs(props)
+
+// Remember replace this with a composable.
+const formatAmount = computed(() => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'MXN',
+  });
+  return formatter.format(amount.value);
+})
 </script>
 
 <template>
   <div class="bg-slate-700 text-white border border-slate-800 w-full h-full flex px-10 py-2 rounded-xl">
     <div class="w-full h-full flex flex-col">
-      <h3 class="text-2xl font-bold">${{ amount }}</h3>
+      <h3 class="text-2xl font-bold">{{ formatAmount }}</h3>
       <span>{{ label }}</span>
     </div>
   </div>
